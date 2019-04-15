aa6343f5c4d210d1754d934959bf80a51adc06db
Wambita Fana <wambitafana@gmail.com>
2019-04-15 19:55:33 +0300

    Add models for the application

---
+++ app/models.py
@@ -0,0 +1,28 @@
+class Source:
+    """
+        Source class that defines a news source objects.
+    """
+    def __init__(self, id, name, category, description):
+        """
+            Function that creates instances of the source objects.
+        """
+        self.id = id
+        self.name = name
+        self.category = category
+        self.description = description
+
+class Article:
+    """
+        Article class that defines article objects.
+    """
+    def __init__(self, id, author, title, image, description, time, url ):
+        """
+            Function that creates instances of the article objects.
+        """
+        self.id = id
+        self.author = author
+        self.title = title
+        self.image = image
+        self.description = description
+        self.time = time
+        self.url = url
---
+++ app/request.py
@@ -0,0 +1,81 @@
+from .models import Source, Article
+from newsapi import NewsApiClient
+
+# Getting the api key
+api_key = None
+
+newsapi = None
+
+def configure_request(app):
+    """
+        Function that retrives the api key and base url from the config file
+
+        Args:
+        app: The flask app instance
+    """
+    global api_key, newsapi
+    api_key = app.config['NEWS_API_KEY']
+    newsapi = NewsApiClient(api_key = api_key)
+    
+def get_sources(category):
+    """
+       Function that gets all the sources 
+    """
+    sources = newsapi.get_sources(category=category)
+
+    results = None
+
+    if sources['status'] == 'ok':
+        sources_list = sources['sources']
+        results = process_results(sources_list)
+
+    return results
+
+def get_articles(source):
+    """
+        Function that get all articles for a specified source.
+    """
+    all_articles = newsapi.get_everything(sources=source)
+
+    results = []
+
+    if all_articles['status'] == 'ok':
+        articles_list = all_articles['articles']
+        for article in articles_list:
+            id = article.get('source')['id']
+            author = article.get('author')
+            title = article.get('title')
+            image = article.get('urlToImage')
+            description = article.get('description')
+            time = article.get('publishedAt')[0:10]
+            url = article.get('url')
+
+            if image:
+                article_object = Article(id, author, title, image, description, time, url)
+                results.append(article_object)
+
+    return results
+
+
+    
+def process_results(sources_list):
+    """
+        Function that processes the sources list and transform them to a list of Objects
+
+      Args:
+        sources_list: A list of dictionaries that contain news source details
+
+      Returns:
+        A list of source objects
+    """
+    sources_results = []
+    for source in sources_list:
+        id = source.get('id')
+        name = source.get('name')
+        category = source.get('category')
+        description = source.get('description')
+
+        source_object = Source(id, name, category, description)
+        sources_results.append(source_object)
+
+    return sources_results
